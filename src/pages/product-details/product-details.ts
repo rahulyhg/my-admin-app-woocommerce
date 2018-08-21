95302e38acccef101043728265f165d9162582c3
poeei_MM <poeeiaerial100@gmail.com>
2018-08-21 16:03:55 +0630

    product delete

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController, ModalController, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, ModalController, LoadingController, AlertController } from 'ionic-angular';
 import * as WC from 'woocommerce-api';
 
 import { WoocommerceProvider } from '../../providers/woocommerce/woocommerce';
@@ -15,7 +15,13 @@ export class ProductDetails {
   WooCommerce: any;
   reviews: any[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, public modalCtrl: ModalController, private WP: WoocommerceProvider, private loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, 
+			  public navParams: NavParams, 
+			  public toastCtrl: ToastController, 
+			  public modalCtrl: ModalController, 
+			  private WP: WoocommerceProvider, 
+			  private alertCtrl: AlertController,
+			  public loadingCtrl: LoadingController) {
 
     this.product = this.navParams.get("product");
     console.log(this.product);
@@ -26,4 +32,35 @@ export class ProductDetails {
     let editProduct = this.product;
 	this.navCtrl.push('EditProductPage', {editProduct});
   }
+  
+  deleteProduct() {
+	
+	  let alert = this.alertCtrl.create({
+		title: 'Delete Product',
+		message: 'Are you sure to delete this product?',
+		buttons: [
+		  {
+			text: 'No',
+			role: 'cancel',
+			handler: () => {
+			  console.log('Cancel clicked');
+			}
+		  },
+		  {
+			text: 'Yes',
+			handler: () => {
+			    this.WooCommerce = this.WP.init();
+				this.WooCommerce.deleteAsync("products/" + this.product.id + "?force=true").then( (data) => {
+				  console.log(JSON.parse(data.body));
+				  this.navCtrl.push("ProductsByCategoryPage", { category : this.product.categories[0]});
+				}, (err) => {
+				  console.log(err);
+				});
+			}
+		  }
+		]
+	  });
+	  alert.present();
+	}
+  
 }
